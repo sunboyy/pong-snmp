53d1ab51e6cd7c511e4337a211c05a849f81092b
sunboyy <sura_sun@hotmail.com>
2019-05-18 12:51:40 +0700

    Allow store constant

--- compiler/scanner.py
+++ compiler/scanner.py
@@ -8,31 +8,44 @@ def tokenize(code: str) -> List[str]:
 
 
 def __tokenize(code: str) -> List[str]:
-    code = code.strip()
-    if len(code) == 0:
-        return []
-    match = re.findall('^[\>\<\=\!]\=', code)
-    if len(match) > 0:
-        token = match[0]
-        return [token] + __tokenize(code[len(token):])
-    match = re.findall('^[<>]{2}', code)
-    if len(match) > 0:
-        token = match[0]
-        return [token] + __tokenize(code[len(token):])
-    match = re.findall('^[;,\(\)\{\}\+\-\*\/\%\&\|\^\~\=\<\>]', code)
-    if len(match) > 0:
-        token = match[0]
-        return [token] + __tokenize(code[len(token):])
-    match = re.findall('^[a-zA-Z_][a-zA-Z_0-9]*', code)
-    if len(match) > 0:
-        token = match[0]
-        return [token] + __tokenize(code[len(token):])
-    match = re.findall('^0x[0-9A-Fa-f]+', code)
-    if len(match) > 0:
-        token = match[0]
-        return [token] + __tokenize(code[len(token):])
-    match = re.findall('^\d+', code)
-    if len(match) > 0:
-        token = match[0]
-        return [token] + __tokenize(code[len(token):])
-    raise SyntaxError('Unknown token ' + code[0])
+    tokens = []
+    while len(code) > 0:
+        code = code.strip()
+        match = re.findall('^[\>\<\=\!]\=', code)
+        if len(match) > 0:
+            token = match[0]
+            tokens.append(token)
+            code = code[len(token):]
+            continue
+        match = re.findall('^[<>]{2}', code)
+        if len(match) > 0:
+            token = match[0]
+            tokens.append(token)
+            code = code[len(token):]
+            continue
+        match = re.findall('^[;,\(\)\{\}\+\-\*\/\%\&\|\^\~\=\<\>]', code)
+        if len(match) > 0:
+            token = match[0]
+            tokens.append(token)
+            code = code[len(token):]
+            continue
+        match = re.findall('^[a-zA-Z_][a-zA-Z_0-9]*', code)
+        if len(match) > 0:
+            token = match[0]
+            tokens.append(token)
+            code = code[len(token):]
+            continue
+        match = re.findall('^0x[0-9A-Fa-f]+', code)
+        if len(match) > 0:
+            token = match[0]
+            tokens.append(token)
+            code = code[len(token):]
+            continue
+        match = re.findall('^\d+', code)
+        if len(match) > 0:
+            token = match[0]
+            tokens.append(token)
+            code = code[len(token):]
+            continue
+        raise SyntaxError('Unknown token ' + code[0])
+    return tokens
